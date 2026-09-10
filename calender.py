52f8660ae11c489026fb39151986771e669f3c1b
chris <cpkrikie@gmail.com>
2026-09-11 00:22:09 +0200

    added leap year logic

--- calender.py
+++ calender.py
@@ -14,29 +14,44 @@ Month = [
     'november',
     'december'
 ]
-
+def leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 i = 0
 year = int(input())
-if year>0:
 
-	for i, month in enumerate(Month):
 
-        	if month in ['january', 'march', 'may', 'july', 'august', 'october', 'december']:
-                	days = 31
+if year > 0:
 
-        	elif month in ['febuary']:
-                	days = 28
+    for i, month in enumerate(Month):
 
-        	else:
-                	days = 30
+        if month in ['january', 'march', 'may', 'july', 'august', 'october', 'december']:
+            days = 31
 
-        	print("\n", Month[i], "\n")
+        elif month in ['febuary']:
+            if leap_year(year):
+                days = 29
+            else:
+                days = 28
 
-        	for day in range(1, days + 1):
-        	        print(day, end=" ")
+        else:
+            days = 30
 
-                	if day % 7 == 0:
-                        	print()
+        print("\n", Month[i], "\n")
+
+        for day in range(1, days + 1):
+            print(day, end=" ")
+
+            if day % 7 == 0:
+                print()
 
 print("\n")
 
